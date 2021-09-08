53a78bfb5827e1ba7f36556784759f960b62af46
Clara <freitasanaclara32@gmail.com>
2021-09-08 09:46:12 -0300

    Criando Titulares

---
+++ MyBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           client clientAna = new client();
+
+           clientAna.titular = "Ana";
+           clientAna.agencia = 1234;
+           clientAna.conta = 789;
+           clientAna.saldo = 100.50;
+
+           client clientClara = new client();
+
+           clientClara.titular = "Clara";
+           clientClara.agencia = 1234;
+           clientClara.conta = 790;
+           clientClara.saldo = 3000;
+
+           Console.WriteLine("O titular desta conta é: "+ clientAna.titular);
+           Console.WriteLine("O titular desta conta é: "+ clientClara.titular);
+        }
+    }
+}
