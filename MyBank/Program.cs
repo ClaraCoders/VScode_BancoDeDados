27c1a7918e0c7bb84dfebcc744b7253c487d9269
Clara <freitasanaclara32@gmail.com>
2021-09-15 09:39:41 -0300

    Add saldo deposito e transferencia

--- MyBank/Program.cs
+++ MyBank/Program.cs
@@ -6,13 +6,21 @@ namespace MyBank
     {
         static void Main(string[] args)
         {
-           client clientAna = new client("Ana", 1234, 789, 100.50);
-           client clientClara = new client("Clara", 1234, 790, 3000);
+           Client clientAna = new Client("Ana", 1234, 789, 100.50);
+           Client clientClara = new Client("Clara", 1234, 790, 3000);
 
-
+           bool retorno = clientAna.Sacar(50);
+           Console.WriteLine(retorno);
            Console.WriteLine("O titular desta conta é: "+ clientAna.titular);
            Console.WriteLine("O titular desta conta é: "+ clientClara.titular);
            
+
+           clientClara.Depositar(3000);
+           Console.WriteLine("O saldo da sua conta foi para R$" + clientClara.saldo);
+
+           clientClara.Transfirir(2000, clientAna);
+           Console.WriteLine("O saldo da sua conta foi para R$" + clientClara.saldo);
+           Console.WriteLine("O saldo da sua conta foi para R$" + clientAna.saldo);
         }
     }
 }
--- MyBank/client.cs
+++ MyBank/client.cs
@@ -1,16 +1,45 @@
-public class client{
+public class Client{
     public string titular;
     public int agencia;
     public int conta;
     public double saldo;
 
-    public client() {}
+    public Client() {}
 
-    public client(string titular, int agencia, int conta, double saldo)
+    public Client(string titular, int agencia, int conta, double saldo)
     {
         this.titular = titular;
         this.agencia = agencia;
         this.conta = conta;
         this.saldo = saldo;
+    }
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else{
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar (double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transfirir (double valor, Client contaDestino)
+    {
+        if(this.saldo < valor){
+            return false;
+        }
+        else{
+            this.Sacar(valor);
+            contaDestino.Depositar(valor);
+            return true;
+        }
     }
 }
